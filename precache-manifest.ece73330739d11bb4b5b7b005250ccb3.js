98abb8f57ba8d0fc5fbdfc338dc751465d538e39
TjFish <34532288+TjFish@users.noreply.github.com>
2020-06-16 18:31:38 +0800

    deploy

---
+++ precache-manifest.ece73330739d11bb4b5b7b005250ccb3.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd5e1fe40b7366423df",
+    "url": "/COVID-19-Dashboard/css/app.4012da4a.css"
+  },
+  {
+    "revision": "e3f286b2b3f93c4289f5",
+    "url": "/COVID-19-Dashboard/css/chunk-vendors.3473e602.css"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/COVID-19-Dashboard/fonts/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/COVID-19-Dashboard/fonts/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/COVID-19-Dashboard/fonts/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/COVID-19-Dashboard/fonts/materialdesignicons-webfont.dc85ceeb.woff2"
+  },
+  {
+    "revision": "06d8bb0a974fd5715b0b54bb4015daa0",
+    "url": "/COVID-19-Dashboard/index.html"
+  },
+  {
+    "revision": "3cd5e1fe40b7366423df",
+    "url": "/COVID-19-Dashboard/js/app.a6522433.js"
+  },
+  {
+    "revision": "e3f286b2b3f93c4289f5",
+    "url": "/COVID-19-Dashboard/js/chunk-vendors.92c9fc07.js"
+  },
+  {
+    "revision": "c6f74ae1bf2d86e38882c250b52aaa5d",
+    "url": "/COVID-19-Dashboard/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/COVID-19-Dashboard/robots.txt"
+  },
+  {
+    "revision": "1cb95374fa8fe96b6b4ec40c7bdf8ea9",
+    "url": "/COVID-19-Dashboard/static/Chart/css/index.css"
+  },
+  {
+    "revision": "2c6ba53e50ff97f7fe297a59ace41bbd",
+    "url": "/COVID-19-Dashboard/static/Chart/index.html"
+  },
+  {
+    "revision": "5d98adc2c09b812c26cb7d74dd50152e",
+    "url": "/COVID-19-Dashboard/static/Chart/js/4add81f750.js"
+  },
+  {
+    "revision": "e22dde40e7db284d6d5a267e3275654c",
+    "url": "/COVID-19-Dashboard/static/Chart/js/data.js"
+  },
+  {
+    "revision": "21c81f7b8e7062fb4b873b69f53ddbc1",
+    "url": "/COVID-19-Dashboard/static/Chart/js/index.js"
+  },
+  {
+    "revision": "43e0a7d0496e085696a01339bceef3df",
+    "url": "/COVID-19-Dashboard/static/Chart/js/react-dom.development.js"
+  },
+  {
+    "revision": "bff81f643a904be8a3d144f7a8142c24",
+    "url": "/COVID-19-Dashboard/static/Chart/js/react.development.js"
+  },
+  {
+    "revision": "0bc0fc8ebaa35dc2e3882540eb533264",
+    "url": "/COVID-19-Dashboard/static/Chart/js/translate.js"
+  },
+  {
+    "revision": "737236dabb037d0488b378a68feff115",
+    "url": "/COVID-19-Dashboard/static/Chart/js/utils.js"
+  },
+  {
+    "revision": "00059c935441d6f79bbd8a18c283cf86",
+    "url": "/COVID-19-Dashboard/static/China/README.md"
+  },
+  {
+    "revision": "8f758d23203d1169f2cfff868f04806a",
+    "url": "/COVID-19-Dashboard/static/China/_config.yml"
+  },
+  {
+    "revision": "079d764df981f1c150665ea7986747ff",
+    "url": "/COVID-19-Dashboard/static/China/getOnsInfo.json"
+  },
+  {
+    "revision": "9a9d630d9c95c6cdc3a257c22341dcd2",
+    "url": "/COVID-19-Dashboard/static/China/index.html"
+  },
+  {
+    "revision": "71a4504d2779be2f3854017ce52618cf",
+    "url": "/COVID-19-Dashboard/static/China/js/echarts.min.js"
+  },
+  {
+    "revision": "2028980be09d280e435a23988747a60d",
+    "url": "/COVID-19-Dashboard/static/China/js/map/china.js"
+  },
+  {
+    "revision": "46ef46f7e69d33a226ccfa1ab2b71bdd",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/anhui.js"
+  },
+  {
+    "revision": "f8d7ab99a6f10f59657e8b8576ac8e8f",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/aomen.js"
+  },
+  {
+    "revision": "95212bfdbce4bb9fb057120512bb760d",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/beijing.js"
+  },
+  {
+    "revision": "812b8ab79433a73966a54bbd2375252d",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/chongqing.js"
+  },
+  {
+    "revision": "e288abf7937b28dbbaca03dd137932cc",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/fujian.js"
+  },
+  {
+    "revision": "f57c460295905e70ee6a660996822288",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/gansu.js"
+  },
+  {
+    "revision": "542c331c970b24a09b5c5aba73eefc93",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/guangdong.js"
+  },
+  {
+    "revision": "ca07ed68d4bd2ed3f81d74017f2ab274",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/guangxi.js"
+  },
+  {
+    "revision": "785856b1b32349e1e13a47bfd61a1646",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/guizhou.js"
+  },
+  {
+    "revision": "0ca3052c2b8ed22a18c325beb309deec",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/hainan.js"
+  },
+  {
+    "revision": "bed65dbff7178542265ad630209fae48",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/hebei.js"
+  },
+  {
+    "revision": "b9fc74245aba9bfe1b5fc1e9de7ed4e2",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/heilongjiang.js"
+  },
+  {
+    "revision": "a460074c4aeff1fccfb1489e839798c0",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/henan.js"
+  },
+  {
+    "revision": "7079255342592890a7e87d988492efd6",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/hubei.js"
+  },
+  {
+    "revision": "de251dd088ee73d233aad8fb17a14e20",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/hunan.js"
+  },
+  {
+    "revision": "7f791cbaab13398f51a70007f42cbd91",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/jiangsu.js"
+  },
+  {
+    "revision": "562216bc80c2b5bf9aa201f17b3fe55e",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/jiangxi.js"
+  },
+  {
+    "revision": "ad930ab317182d42074307c8d8fbd24f",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/jilin.js"
+  },
+  {
+    "revision": "d1cef4b37ef7a3b78f83b6d3cdd9c9c0",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/liaoning.js"
+  },
+  {
+    "revision": "ebd2711a4203c7366d1a7daba842c251",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/neimenggu.js"
+  },
+  {
+    "revision": "7d7b0015c845cdb7189db5d0ff619520",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/ningxia.js"
+  },
+  {
+    "revision": "8465b53f0623ce0684fe087921419461",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/qinghai.js"
+  },
+  {
+    "revision": "db8f8c73e8cc8a22100c16cb73184bf9",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/shandong.js"
+  },
+  {
+    "revision": "9557109440226aa62dc72e26b606c600",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/shanghai.js"
+  },
+  {
+    "revision": "821d850b83c78db16819b20d7d8d272c",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/shanxi.js"
+  },
+  {
+    "revision": "51d941cb571216183737428a0a938d25",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/shanxi1.js"
+  },
+  {
+    "revision": "29df185ca829675d0e08780169ff6265",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/sichuan.js"
+  },
+  {
+    "revision": "1cff78aa456d6e192c84029f58bc2965",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/taiwan.js"
+  },
+  {
+    "revision": "87474bdeec6fa7e7647b6cb80cd3edc5",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/tianjin.js"
+  },
+  {
+    "revision": "63231a50394e1ac7c2aead892924564e",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/xianggang.js"
+  },
+  {
+    "revision": "96973c8776c0ff89456d4cf111842723",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/xinjiang.js"
+  },
+  {
+    "revision": "f41c6fadb6fe27b08d00352a0c657f42",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/xizang.js"
+  },
+  {
+    "revision": "d4f1a0d8af7d36e6d22cf957e65c1224",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/yunnan.js"
+  },
+  {
+    "revision": "8631a5318db56cf1d06bce4f498e62dc",
+    "url": "/COVID-19-Dashboard/static/China/js/map/province/zhejiang.js"
+  },
+  {
+    "revision": "e63531350b726384f625ead641f5ad66",
+    "url": "/COVID-19-Dashboard/static/China/libs/axios.min.js"
+  },
+  {
+    "revision": "a365baf8434ecd2c6f0763223a73a7eb",
+    "url": "/COVID-19-Dashboard/static/China/province.json"
+  },
+  {
+    "revision": "a3cf38959b58c9f58781e0fcad8dc303",
+    "url": "/COVID-19-Dashboard/static/Ranking/color_ranges.js"
+  },
+  {
+    "revision": "d1ad6c65e80c460a7d63ad7b882a6684",
+    "url": "/COVID-19-Dashboard/static/Ranking/config.js"
+  },
+  {
+    "revision": "6a8f39b17e3db620658df82567cc9451",
+    "url": "/COVID-19-Dashboard/static/Ranking/imgs.js"
+  },
+  {
+    "revision": "7bdae903e4e70119c5785ca9822e4f5c",
+    "url": "/COVID-19-Dashboard/static/Ranking/index.html"
+  },
+  {
+    "revision": "3a5aabc7c7406ee9e018800619f0ff86",
+    "url": "/COVID-19-Dashboard/static/Ranking/nCov_china_0319.csv"
+  },
+  {
+    "revision": "bb8c0784a8aed2b3c017c2d2c72c55d8",
+    "url": "/COVID-19-Dashboard/static/Ranking/nCov_world_0516.csv"
+  },
+  {
+    "revision": "a2005e7e1fdf3be00f051ae26bc96bd9",
+    "url": "/COVID-19-Dashboard/static/Ranking/nCov_wuhan_0319.csv"
+  },
+  {
+    "revision": "d9f62c35b5c966d2a08d0d6aeb3f9169",
+    "url": "/COVID-19-Dashboard/static/Ranking/stylesheet.css"
+  },
+  {
+    "revision": "cc8c94342b4295d3dd8ba450b4916670",
+    "url": "/COVID-19-Dashboard/static/Ranking/university.csv"
+  },
+  {
+    "revision": "2b898163841edbfccd344792fad2e32f",
+    "url": "/COVID-19-Dashboard/static/Ranking/university_ranking.csv"
+  },
+  {
+    "revision": "d533b23322a9f5049cc6131c69c1a357",
+    "url": "/COVID-19-Dashboard/static/Ranking/visual.js"
+  }
+]);
